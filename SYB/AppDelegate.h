a265d9ac4d984efd820a942611035c438778ee7f
WangJincai <apple@192.168.3.9>
2016-06-13 11:00:01 +0800

    Initial Commit

---
+++ SYB/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SYB
+//
+//  Created by WangJincai on 16/6/13.
+//  Copyright © 2016年 WJC.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
